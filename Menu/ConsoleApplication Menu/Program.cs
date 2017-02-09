4578698968d3e3760ae914a0be7fe36d3ebd65b1
Smetanski <rozenheim@mail.rugit config --global user.email rozenheim@mail.ru>
2017-02-09 21:48:22 +0200

    Added lesson 2-3

---
+++ Menu/ConsoleApplication Menu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p1 = "Sup";
+            string p2 = "Beer";
+            string p3 = "Beacon";
+            float price1 = 14.99f;
+            float price2 = 17.99f;
+            float price3 = 18.99f;
+            string waiter = "Sem";
+            float sum = price1 + price2 + price3; 
+           
+            Console.WriteLine("Bill: " + waiter);
+            Console.WriteLine(p1 +"    " + "$" + price1 );
+            Console.WriteLine(p2 +"    " + "$" + price2);
+            Console.WriteLine(p3 +"    " + "$" + price3);
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("Sum    " +  sum);
+
+
+
+
+
+        }
+    }
+}
