e2ca18db55435521ee75836425409b6dca99299b
Smetanski <rozenheim@mail.ru>
2017-02-10 09:17:09 +0200

    Added leson 2-3

---
+++ lesson 2/lesson 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number of kilometers");
+            float km = float.Parse(Console.ReadLine());
+
+            float m = km * 1000;
+
+            Console.WriteLine("{0} km = {1} m ", km, m);
+            Console.WriteLine($"{km} km = {m} m");
+            Console.WriteLine(km + " km =  " + m + " m");
+            Console.Write("Enter Pin:****\b\b\b\b");
+            int pin = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
