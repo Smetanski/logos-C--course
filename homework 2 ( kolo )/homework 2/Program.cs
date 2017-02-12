10f5889c6454ebecad43929f9f02e47fa260d715
Smetanski <rozenheim@mail.ru>
2017-02-12 14:12:07 +0200

    Added homework 2

---
+++ homework 2 ( kolo )/homework 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Circle Calculator");
+            Console.WriteLine("Enter radius");
+          double r = double.Parse(Console.ReadLine());
+            double c;
+            double pi = Math.PI;
+
+            c =  2 * pi * r;
+
+            Console.WriteLine("R=" + r);
+            Console.WriteLine("C= {0}", c );
+        }
+    }
+}
